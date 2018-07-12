8c3bdb3b6b36ca40bd0181bd690c2dc1fed8ed32
candy <1964646121@qq.com>
2018-07-12 11:22:45 +0800

    配置grunt,实现服务自动重启

---
+++ gruntfile.js
@@ -0,0 +1,102 @@
+module.exports=function (grunt) {
+  grunt.initConfig({
+    watch: {
+      jade: {
+        files: ['views/**'],
+        options: {
+          livereload: true   // livereload设为ture, 则当被监听的文件发生改变时将重新启动服务
+        }
+      },
+      js: {
+        files: ['public/js/**', 'models/**/*.js', 'schemas/**/*.js'],
+        //tasks: ['jshint'],  // 语法检查
+        options: {
+          livereload: true
+        }
+      },
+      uglify: {
+        files: ['public/**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          livereload: true
+        }
+      },
+      styles: {
+        files: ['public/**/*.less'],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        ignores: ['public/libs/**/*.js']
+      },
+      all: ['public/js/*.js', 'test/**/*.js', 'app/**/*.js']
+    },
+
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          'public/build/index.css': 'public/less/index.less'
+        }
+      }
+    },
+
+    uglify: {
+      development: {
+        files: {
+          'public/build/admin.min.js': 'public/js/admin.js',
+          'public/build/detail.min.js': [
+            'public/js/detail.js'
+          ]
+        }
+      }
+    },
+
+    nodemon: {
+      dev: {    // dev,开发环境
+        options: {
+          file: 'app.js',
+          args: [],
+          ignoredFiles: ['README.md', 'node_modules/**', '.DS_Store'],
+          watchedExtensions: ['js'],
+          watchedFolders: ['./'],
+          debug: true,
+          delayTime: 1,  // 文件发生变动时，等待多少毫秒才重启
+          env: {
+            PORT: 3000
+          },
+          cwd: __dirname,   // 目录配置为当前目录
+        }
+      }
+    },
+
+    mochaTest: {
+      options: {
+        reporter: 'spec'
+      },
+      src: ['test/**/*.js']
+    },
+
+    concurrent: {
+      tasks: ['nodemon', 'watch', 'less', 'uglify', 'jshint'],  // 通过tasks 传入5个任务
+      options: {
+        logConcurrentOutput: true
+      }
+    }
+  })
+  grunt.loadNpmTasks("grunt-contrib-watch")  //grunt-contrib-watch插件，只要有文件发生变动，便会执行注册好的相应任务
+  grunt.loadNpmTasks("grunt-nodemon")  //grunt-nodemon插件，实时监听app.js文件，发生变动则重启
+  grunt.loadNpmTasks("grunt-concurrent")  //grunt-concurrent，针对慢任务（如sass,less编译）的插件，优化构建时间
+  grunt.option('force',true)  //设为ture,则不会因为一些语法错误/警告而中断整个grunt任务
+  grunt.registerTask("default",['concurrent'])  //注册一个concurrent任务
+}
