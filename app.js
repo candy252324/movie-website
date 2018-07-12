4865780f8c3a9c33423b3f627f138b8d47344396
candy <1964646121@qq.com>
2018-07-12 15:08:18 +0800

    实现用户注册

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var express=require('express')
 var path=require('path')
 var mongoose=require("mongoose")
 var Movie=require("./models/movie")
+var User=require("./models/user")
 var _=require('underscore')
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser')  // 中间件，将post请求body中的内容格式化为一个对象
 var port=process.env.PORT||3000
 var app=express()   //启动web服务器
 
@@ -41,6 +42,47 @@ app.get('/',function (req,res) {
   })
 
 })
+
+// 注册
+app.post('/user/signup',function (req,res) {
+  var _user=req.body.user;
+  //查找用户名是否重复
+  User.find({name:_user.name},function (err,user) {
+    if(err){
+      console.log(err)
+    }
+    //重复
+    if(user){
+      return res.redirect('/')
+      //不重复
+    }else{
+      var user=new User(_user);    // 创建一条新数据
+      user.save(function (err,user) {
+        if(err){
+          console.log(err)
+        }
+        res.redirect("/admin/userList")    // 重定向到首页
+      })
+    }
+  })
+
+})
+
+app.get('/admin/userList',function (req,res) {
+  User.fetch((err,doc)=>{
+    if(err){
+      console.log(err)
+    }
+    res.render('userList',{
+      title:"用户列表页",
+      users:doc,
+    })
+  })
+})
+
+
+
+
 app.get('/movie/:id',function (req,res) {
   var id=req.params.id
   Movie.findById(id,(err,doc)=>{
@@ -51,6 +93,8 @@ app.get('/movie/:id',function (req,res) {
   })
 
 })
+
+//后台录入页
 app.get('/admin/movie',function (req,res) {
   res.render('admin',{
     title:"后台录入页",
@@ -67,6 +111,7 @@ app.get('/admin/movie',function (req,res) {
   })
 })
 
+// 后台修改页
 app.get('/admin/update/:id',(req,res)=>{
   var id=req.params.id;
   if(id){
@@ -79,6 +124,7 @@ app.get('/admin/update/:id',(req,res)=>{
   }
 })
 
+// 后台录入post提交
 app.post("/admin/movie/new",(req,res)=>{
   var id=req.body.movie._id;
   var movieObj=req.body.movie;
---
+++ schemas/user.js
@@ -0,0 +1,65 @@
+var mongoose=require("mongoose")
+var bcrypt=require("bcrypt")
+var SALT_WORK_FACTOR=10  // 加密计算强度设为10（默认值也为10）
+
+
+var UserSchema=new mongoose.Schema({
+  name:{
+    unique:true,   // 表示不可重复
+    type:String,
+  },
+  password:String,
+  meta:{
+    createAt:{
+      type:Date,
+      default:Date.now(),
+    },
+    updateAt:{
+      type:Date,
+      default:Date.now(),
+    }
+  },
+})
+
+//pre save 每次存储数据之前都要调用这个方法
+UserSchema.pre('save',function(next){
+
+  var user=this; // this及当前这个user
+
+  //若为新增数据，则把它的创建时间和更新时间设置为当前时间
+  if(this.isNew){
+    this.meta.createAt=this.meta.updateAt=Date.now();
+  }else{
+    this.meta.updateAt=Date.now();
+  }
+  // 生成一个随机的盐
+  //SALT_WORK_FACTOR参数为计算强度，计算强度越大，攻击者建立彩虹表就越困难，破解越困难
+  bcrypt.genSalt(SALT_WORK_FACTOR,function (err,salt) {
+    if(err) return next(err)
+    // hash加密，bcrypt.hash接收三个参数，第一个为明文密码，第二个为盐，第三个为回调
+    bcrypt.hash(user.password,salt,function (err,hash) {
+      if(err) return next(err)
+      user.password=hash;   // 将最终加密过后的密码保存到user.password
+      next();  // next 必须写在回调里，若写在bcrypt.genSalt外面，将无法加密
+    })
+  })
+})
+
+//添加静态方法
+UserSchema.statics={
+  //fetch,用于查找出所有数据并按updateAt进行排序
+  fetch:function (cb) {
+    return this
+      .find({})
+      .sort('meta.updateAt')
+      .exec(cb)
+  },
+  //findById用于查找指定id的数据
+  findById:function (id,cb) {
+    return this
+      .findOne({_id:id})
+      .exec(cb)
+  }
+}
+//导出模式
+module.exports=UserSchema;
