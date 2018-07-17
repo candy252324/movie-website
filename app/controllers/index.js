cce29d7626efb58a8243224bded0a8e6c95408a0
candy <1964646121@qq.com>
2018-07-17 10:47:36 +0800

    增加搜索功能

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -22,33 +22,58 @@ exports.index=function (req,res) {
 
 //search
 exports.search=function (req,res) {
+  console.log("111111111111111111111111111111111")
+  console.log(req.query)
   var catId=req.query.cat;
-  var page=parseInt(req.query.p);
+  var q=req.query.q;  // 搜索框内容
+  var page=parseInt(req.query.p,10)||0;  // 搜索框搜索时，默认给0
   var count=2;  // 每页数据条数
   var index=page*count;
-
-  Category
-    .find({_id:catId})
-    .populate({
-      path:'movies',
-      select:'title poster',
-      // options:{limit:6,skip:index}  // 如此分页有bug
-    })
-    .exec(function (err,categories) {
-      if(err){
-        console.log(err)
-      }
-      var category=categories[0]||{};
-      var movies=category.movies||[];
-      var results=movies.slice(index,index+count) // 分页数据
-
-      res.render('results',{
-        title:"结果列表页面",
-        keyword:category.name,   // 用于搜索
-        currentPage:page+1,
-        query:'cat='+catId,
-        totalPage:Math.ceil(movies.length/count),  // 总页数
-        movies:results,
+  // 某电影分类里的分页搜索
+  if(catId){
+    Category
+      .find({_id:catId})
+      .populate({
+        path:'movies',
+        select:'title poster',
+        // options:{limit:6,skip:index}  // 如此分页有bug
       })
-    })
+      .exec(function (err,categories) {
+        if(err){
+          console.log(err)
+        }
+        var category=categories[0]||{};
+        var movies=category.movies||[];
+        var results=movies.slice(index,index+count) // 分页数据
+
+        res.render('results',{
+          title:"结果列表页面",
+          keyword:category.name,  // 搜索的分类名称
+          currentPage:page+1,
+          query:'cat='+catId,
+          totalPage:Math.ceil(movies.length/count),  // 总页数
+          movies:results,
+        })
+      })
+  }
+  //搜索框搜索
+  else{
+    Movie
+      .find({title: new RegExp(q + '.*','i')})   // 使用正则，模糊匹配
+      .exec(function (err,movies) {
+        if(err){
+          console.log(err)
+        }
+        var results=movies.slice(index,index+count) // 分页数据
+
+        res.render('results',{
+          title:"结果列表页面",
+          keyword:q,   // 搜索框内容
+          currentPage:page+1,
+          query:'q='+q,
+          totalPage:Math.ceil(movies.length/count),  // 总页数
+          movies:results,
+        })
+      })
+  }
 }
